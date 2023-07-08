088af3e754f36df57fd0706631393648b2418da2
Raashi M Y <111100699+myraashi@users.noreply.github.com>
2023-07-08 13:10:30 +0530

    Create 10a.php

---
+++ 10a.php
@@ -0,0 +1,10 @@
+<?php
+// Set the default timezone to your desired timezone
+date_default_timezone_set('Asia/Kolkata');
+
+// Get today's date
+$today = date("d-m-Y");
+
+// Display the date
+echo "Today's date is: " . $today;
+?>
